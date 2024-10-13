954c7985a650bcd7cd8d29bc5838e47955f8f7dd
Patryk Czartowski <patrykczartowski6@gmail.com>
2024-10-13 20:36:53 +0200

    Sign up form validation and processing

--- EncChat/src/components/Forms/LoginForm/LoginForm.jsx
+++ EncChat/src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,14 +1,30 @@
 import './LoginForm.module.css';
 
 export default function LoginForm() {
+
+    const processLogin = (event) => {
+        event.preventDefault();
+        console.log("Login form submitted");
+        const username = event.target[0].value;
+        const password = event.target[1].value;
+        const usernameIsEmail = username.includes('@');
+        
+        const loginData = {
+            username: username,
+            password: password,
+            usernameIsEmail: usernameIsEmail
+        }
+        console.log(loginData);
+    }
+
     return (
         <div className="login_form">
             <h2>Login</h2>
-            <form>
+            <form onSubmit={processLogin}>
                 <label>Username</label>
-                <input type="text" placeholder="Enter your username" />
+                <input id="username" name="username" type="text" placeholder="Enter your username" />
                 <label>Password</label>
-                <input type="password" placeholder="Enter your password" />
+                <input id="password" name="password" type="password" placeholder="Enter your password" />
                 <a href="#">Forgot password?</a>
                 <p>WIP reCaptha</p>
                 <button type="submit">Login</button>
--- EncChat/src/components/Forms/SignupForm/SignupForm.jsx
+++ EncChat/src/components/Forms/SignupForm/SignupForm.jsx
@@ -1,11 +1,72 @@
-import './Signup.module.css';
+import { useState } from 'react';
 
-export default function SignupForm() {
+import './SignUp.module.css';
+
+export default function SignUpForm() {
+
+    const [isUsernameValid, setIsUsernameValid] = useState(true);
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [isEmailValid, setIsEmailValid] = useState(true);
+
+    const specialCharPattern = /[!@#$%^&*(),.?":{}|<>]/;
+    const numberPattern = /[0-9]/;
+    const upperCasePattern = /[A-Z]/;
+    const lowerCasePattern = /[a-z]/;
+
+    const processSignUp = (event) => {
+        event.preventDefault();
+        console.log("Sign up form submitted");
+        const signUpData = {
+            username: event.target[0].value,
+            password: event.target[1].value,
+            email: event.target[2].value
+        }
+        console.log(signUpData);
+    }
+
+    const validateUsername = (username) => {
+        const usernameHasSpecialChars = specialCharPattern.test(username);
+        setIsUsernameValid(!usernameHasSpecialChars);
+        return !usernameHasSpecialChars;
+    }
+
+    const validatePassword = (password) => {
+        const passwordHasSpecialChars = specialCharPattern.test(password);
+        const passwordHasNumber = numberPattern.test(password);
+        const passwordHasUpperCase = upperCasePattern.test(password);
+        const passwordHasLowerCase = lowerCasePattern.test(password);
+        const passwordIsValid = passwordHasSpecialChars && passwordHasNumber && passwordHasUpperCase && passwordHasLowerCase;
+        
+        setIsPasswordValid(passwordIsValid);
+        return passwordIsValid;
+    }
+
+    const validateEmail = (email) => {
+        const emailIsValid = email.includes('@');
+        setIsEmailValid(emailIsValid);
+        return emailIsEmail;
+    }
+
+    const validateSignUp = (event) => {
+        event.preventDefault();
+        const username = event.target[0].value;
+        const password = event.target[1].value;
+        const email = event.target[2].value;
+        
+        const usernameIsValid = () => validateUsername(username);
+        const passwordIsValid = () => validatePassword(password);
+        const emailIsValid = () => validateEmail(email);
+
+        if (usernameIsValid && passwordIsValid && emailIsValid) {
+            processSignUp(event);
+        }
+
+    }
 
     return (
-        <div className="signup_form">
+        <div className="signUpForm">
             <h2>Sign up</h2>
-            <form>
+            <form onSubmit={processSignUp}>
                 <label>Username</label>
                 <input type="text" placeholder="Enter your username" />
                 <label>Password</label>
